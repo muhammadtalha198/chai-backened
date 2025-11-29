c3895b1cf63aa7b32f24ca60b21f5b430497ca39
Muhammad <mrmacbook@MrMacBooks-MacBook-Pro.local>
2025-11-29 11:56:27 +0500

    all basic code requires is here

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import dotenv from "dotenv"
 import connectBD from "./db/index.js";
 dotenv.config()
 
-connectBD()
+connectBD().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}).catch((error) => {
+    console.error("Database connection failed", error);
+    process.exit(1);
+});
 
 
 
